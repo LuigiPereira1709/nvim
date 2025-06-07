e6585f3ccfb71f0a546a964c1e72a1952442a129
luigiPereira <luigipereira1001@gmail.com>
2025-06-07 16:30:55 -0300

    42

--- init.lua
+++ init.lua
@@ -1,35 +1,37 @@
-require "core.globals"
+require("core.globals")
+
+vim.o.termguicolors = true
 
 if vim.version().minor >= 11 then
-  vim.tbl_add_reverse_lookup = function(tbl)
-    for k, v in pairs(tbl) do
-    tbl[v] = k
-    end
-  end
+	vim.tbl_add_reverse_lookup = function(tbl)
+		for k, v in pairs(tbl) do
+			tbl[v] = k
+		end
+	end
 end
 
 -- bootstrap lazy and all plugins
-local lazypath = vim.fn.stdpath "data" .. "/lazy/lazy.nvim"
+local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
 
 if not vim.loop.fs_stat(lazypath) then
-  local repo = "https://github.com/folke/lazy.nvim.git"
-  vim.fn.system { "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath }
+	local repo = "https://github.com/folke/lazy.nvim.git"
+	vim.fn.system({ "git", "clone", "--filter=blob:none", repo, "--branch=stable", lazypath })
 end
 
 vim.opt.runtimepath:prepend(lazypath)
 
 -- NOTE: lazy.nvim options
-local lazy_config = require "core.lazy"
+local lazy_config = require("core.lazy")
 
 -- NOTE: Load plugins
 require("lazy").setup({
-  { import = "plugins" },
+	{ import = "plugins" },
 }, lazy_config)
 
-require "options"
+require("options")
 
 vim.schedule(function()
-  require "mappings"
+	require("mappings")
 end)
 
-require "myinit"
+require("myinit")
