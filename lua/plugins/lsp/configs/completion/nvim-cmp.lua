104c122dbd7913357ca039390eb2ba8ec493f8e3
pitanguinha <luigipereira1001@gmail.com>
2025-09-26 15:00:54 -0300

    42

--- lua/plugins/lsp/configs/completion/nvim-cmp.lua
+++ lua/plugins/lsp/configs/completion/nvim-cmp.lua
@@ -8,14 +8,12 @@ return {
 		},
 	},
 	{
-		"hrsh7th/cmp-nvim-lsp",
-	},
-	{
 		"hrsh7th/nvim-cmp",
 		event = { "InsertEnter" },
 		dependencies = {
 			"hrsh7th/cmp-buffer",
 			"hrsh7th/cmp-path",
+			"hrsh7th/cmp-nvim-lsp",
 		},
 		init = function()
 			vim.keymap.set("n", "<leader>oa", function()
